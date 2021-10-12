d1e869758a70508657b4173a9dae4e32e768672a
jukkanghost2 <80653155+jukkanghost2@users.noreply.github.com>
2021-10-12 19:13:07 -0300

    rx y tx

--- sources_1/new/UART_RX.v
+++ sources_1/new/UART_RX.v
@@ -22,7 +22,7 @@
 
 module UART_RX
 #(
-      //PARAMETERS
+   //PARAMETERS
    )
   (
   //INPUTS
@@ -35,20 +35,21 @@ module UART_RX
    );
    
   // One-Hot, One-Cold  
-  parameter STATE_IDLE         = 4'b0001;
-  parameter STATE_START_BIT    = 4'b0010;
-  parameter STATE_RECEIVING    = 4'b0100;
-  parameter STATE_STOP_BIT     = 4'b1000;
+  localparam STATE_IDLE         = 4'b0001;
+  localparam STATE_START_BIT    = 4'b0010;
+  localparam STATE_RECEIVING    = 4'b0100;
+  localparam STATE_STOP_BIT     = 4'b1000;
    
   //reg           r_Rx_Data_R = 1'b1;
   reg           rx_data   = 1'b1;
    
-  reg [7:0]     r_Clock_Count  = 0;
+  reg [7:0]     tick_counter  = 0;
   reg [2:0]     data_index     = 0; //8 bits total
   reg [7:0]     data_byte      = 0;
   reg           done_bit       = 0;
-  reg [1:0]     current_state  = 0;
-  reg [1:0]     next_state     = 0;
+  reg [3:0]     current_state  = 0;
+  reg [3:0]     next_state     = 0;
+  
 
    assign  o_done_bit  =  done_bit;
    
@@ -66,6 +67,7 @@ module UART_RX
         STATE_IDLE:
         begin
             data_index <= 0;
+            tick_counter <= 0;
             if(rx_data == 1'b0) //Start bit detected
                 next_state <= STATE_START_BIT;
             else
@@ -74,30 +76,60 @@ module UART_RX
         
         STATE_START_BIT:
         begin
-            if(rx_data == 1'b0) //Start bit still low
-                next_state <= STATE_RECEIVING;
+            if(tick_counter == 7)
+             begin
+                if(rx_data == 1'b0) //Start bit still low
+                begin
+                    tick_counter <= 0; //(found middle, reset counter)
+                    next_state <= STATE_RECEIVING;
+                end
+                else
+                    next_state <= STATE_IDLE;
+             end
             else
-                next_state <= STATE_IDLE;
+             begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_START_BIT;
+             end
         end
         
         STATE_RECEIVING:
         begin
-            if(data_index < 7)
-                begin
-                    data_byte[data_index] <= rx_data;
-                    data_index <= data_index + 1;
-                    next_state <= STATE_RECEIVING;
-                end
+            if(tick_counter < 15)
+             begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_RECEIVING;
+             end
             else
-                begin
+             begin
+                tick_counter <= 0;
+                data_byte[data_index] <= rx_data;
+                
+                if(data_index < 7)
+                 begin  
+                        data_index <= data_index + 1;
+                        next_state <= STATE_RECEIVING;
+                 end
+            else
+             begin
                     data_index <= 0;
                     next_state <= STATE_STOP_BIT;
-                end
+              end
+             end
         end
         
         STATE_STOP_BIT:
         begin
+            if(tick_counter < 15)
+             begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_STOP_BIT;
+             end
+            else
+             begin
+                tick_counter <= 0;
                 next_state <= STATE_IDLE;
+             end
         end
               
         default:
--- sources_1/new/UART_TX.v
+++ sources_1/new/UART_TX.v
@@ -35,20 +35,20 @@ module UART_TX
    );
    
   // One-Hot, One-Cold  
-  parameter STATE_IDLE         = 4'b0001;
-  parameter STATE_START_BIT    = 4'b0010;
-  parameter STATE_TRANSMITTING = 4'b0100;
-  parameter STATE_STOP_BIT     = 4'b1000;
+  localparam STATE_IDLE         = 4'b0001;
+  localparam STATE_START_BIT    = 4'b0010;
+  localparam STATE_TRANSMITTING = 4'b0100;
+  localparam STATE_STOP_BIT     = 4'b1000;
    
   //reg           r_Rx_Data_R = 1'b1;
   reg           tx_data   = 1'b1;
    
-  reg [7:0]     r_clock_count  = 0;
+  reg [7:0]     tick_counter  = 0;
   reg [2:0]     data_index     = 0; //8 bits total
   reg [7:0]     data_byte      = 0;
   reg           done_bit       = 0;
-  reg [1:0]     current_state  = 0;
-  reg [1:0]     next_state     = 0;
+  reg [3:0]     current_state  = 0;
+  reg [3:0]     next_state     = 0;
 
    assign  o_done_bit  =  done_bit;
    
@@ -66,6 +66,7 @@ module UART_TX
         STATE_IDLE:
         begin
             data_index <= 0;
+            tick_counter <= 0;
             if(tx_data == 1'b0) //Start bit detected
                 next_state <= STATE_START_BIT;
             else
@@ -74,30 +75,60 @@ module UART_TX
         
         STATE_START_BIT:
         begin
-            if(tx_data == 1'b0) //Start bit still low
-                next_state <= STATE_TRANSMITTING;
-            else
-                next_state <= STATE_IDLE;
+            if(tick_counter == 7)
+             begin
+                if(tx_data == 1'b0) //Start bit still low
+                 begin
+                    tick_counter <= 0; //(found middle, reset counter)
+                    next_state <= STATE_TRANSMITTING;
+                 end
+                else
+                     next_state <= STATE_IDLE;
+              end
+             else
+              begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_START_BIT;
+              end
         end
         
         STATE_TRANSMITTING:
         begin
-            if(data_index < 7)
-                begin
-                    data_byte[data_index] <= tx_data;
-                    data_index <= data_index + 1;
-                    next_state <= STATE_TRANSMITTING;
-                end
+            if(tick_counter < 15)
+             begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_TRANSMITTING;
+             end
             else
-                begin
-                    data_index <= 0;
-                    next_state <= STATE_STOP_BIT;
-                end
+             begin
+                tick_counter <= 0;
+                data_byte[data_index] <= tx_data;
+                            
+                if(data_index < 7)
+                 begin
+                        data_index <= data_index + 1;
+                        next_state <= STATE_TRANSMITTING;
+                 end
+                else
+                 begin
+                        data_index <= 0;
+                        next_state <= STATE_STOP_BIT;
+                 end
+             end
         end
         
         STATE_STOP_BIT:
         begin
+            if(tick_counter < 15)
+             begin
+                tick_counter <= tick_counter + 1;
+                next_state <= STATE_STOP_BIT;
+             end
+            else
+             begin
+                tick_counter <= 0;
                 next_state <= STATE_IDLE;
+             end
         end
               
         default:
